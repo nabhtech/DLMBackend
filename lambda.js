69f8614e173f235086d2a2f1e78929006688eb9d
Ravi-khamla <ravikhamladeveloper@gmail.com>
2022-12-15 15:52:00 +0530

    fixing cors issue attempt 3

--- lambda.js
+++ lambda.js
@@ -3,13 +3,4 @@ const awsServerlessExpress = require('aws-serverless-express')
 const app = require('./app')
 const server = awsServerlessExpress.createServer(app)
 
-exports.handler = (event, context, callback) =>{ 
-    const response = {
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Credentials': true,
-        },
-      };
-    awsServerlessExpress.proxy(server, event, context)
-    callback(null, response)
-}
+exports.handler = (event, context) => awsServerlessExpress.proxy(server, event, context)
