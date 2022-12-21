e6082d373d62400dfca2e2efc7d72f9555ab50fe
Ravi-khamla <ravikhamladeveloper@gmail.com>
2022-12-21 16:29:01 +0530

    updating cors issue

--- lambda.js
+++ lambda.js
@@ -1,6 +1,24 @@
 'use strict'
 const awsServerlessExpress = require('aws-serverless-express')
 const app = require('./app')
+const { leaderBoard } = require('../controllers/studentTestController');
 const server = awsServerlessExpress.createServer(app)
+
+exports.getLeaderboard = async (event, context, callback) => {
+    const leaderboard = await leaderBoard(event)
+
+    const response = {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Credentials': true,
+        },
+        body: JSON.stringify({
+            leaderboard: leaderboard
+        }),
+      };
+     
+      callback(null, response);
+}
 
 exports.handler = (event, context) => awsServerlessExpress.proxy(server, event, context)
